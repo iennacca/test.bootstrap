5d579e1354f58e1d4137bb6d089e53efdf412198
iennacca <iennacca@yahoo.com>
2014-08-09 20:05:03 -0400

    Changes to infrastructure

--- cdo/infrastructure.ts
+++ cdo/infrastructure.ts
@@ -21,7 +21,7 @@ interface AsyncDataSource {
 
 
 interface AsyncDataTransform {
-    TransformAsync(data: CDOData): JQueryPromise<CDOData>;
+    TransformDataAsync(data: CDOData): JQueryPromise<CDOData>;
 }
 
 class DP implements AsyncDataSource {
@@ -29,34 +29,39 @@ class DP implements AsyncDataSource {
         var d = $.Deferred();
 
         setTimeout(function() {
-            var data = new CDOData();
-            data.Id = 1;
-            data.Name = 'Jerry';
+            var dpData = new CDOData();
+            dpData.Id = 1;
+            dpData.Name = 'Jerry';
+            $('#result1').html(dpData.Name);
+            d.resolve(dpData);
         }, 2000);
         return d.promise();
     }
 }
 
 class TP implements AsyncDataTransform {
-    TransformAsync(data: CDOData): JQueryPromise<CDOData> {
+    TransformDataAsync(tData: CDOData): JQueryPromise<CDOData> {
         var d = $.Deferred();
 
         setTimeout(function() {
-            console.log(data.Name);
+            tData.Name = tData.Name + ' Chaves';
+            $('#result2').html(tData.Name);
+            d.resolve(tData);
         }, 2000);
         return d.promise();
     }
 }
 
-
-
 function Visualizer() {
-    var process: JQueryDeferred<CDOData>;
-    var dataPromise: JQueryPromise<CDOData>;
-    var transformPromise: JQueryPromise<TransformData>;
+    var process: JQueryDeferred<CDOData> = $.Deferred();
 
     process
         .then(function() { return new DP().GetDataAsync(); })
-        .then(function(data) { return new TP().TransformAsync(data); })
-        .done();
+        .then(function(d1) { return new TP().TransformDataAsync(d1); })
+        .then(function(d2) { console.log(d2) })
+        .done(function() {
+            $('#done').html('Done.');
+        });
+
+    process.resolve();
 }
